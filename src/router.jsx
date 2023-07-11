17d58089742f5ea99de130da0902567d0e17eb0b
Vyacheslav Kartashov <kartashov.developer@gmail.com>
2023-07-11 18:49:55 +0300

    Created some structure for future app.

--- src/router.jsx
+++ src/router.jsx
@@ -3,6 +3,17 @@ import { Navigate } from 'react-router-dom'
 const routes = [
   {
     path: '',
+    element: <BaseLayout />,
+    children: [
+        {
+            path: '/',
+            element: <Overview />
+        },
+        {
+            path: 'overview',
+            element: <Navigate to='/' replace />
+        },
+    ]
   },
 ]
 
