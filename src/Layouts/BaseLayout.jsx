0a576d8cd061600ad2fe01e32eab6da6333c9daf
Vyacheslav Kartashov <kartashov.developer@gmail.com>
2023-07-19 11:28:00 +0300

    Modified BaseLayout

--- src/Layouts/BaseLayout.jsx
+++ src/Layouts/BaseLayout.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 // Router
 import { Outlet } from 'react-router'
 // MUI
-import { Box, Button, Typography, Container } from '@mui/material/'
-import { shadows } from '@mui/system';
+import { Container } from '@mui/material/'
 // Components
 import Info from './Info/Info'
 import Search from './Search/Search'
@@ -14,20 +13,37 @@ export default function BaseLayout({ children }) {
     return (
         <>
             {/* Background */}
-            <Container maxWidth='100' disableGutters sx={{
+            <Container 
+              maxWidth='100' 
+              disableGutters 
+              sx={{
                 position: 'absolute',
                 backgroundColor: '#F9F9F9',
                 height: '100%',
                 width: '100%',
                 zIndex: -1000,
-            }}></Container>
+              }}></Container>
             {/* Line for Navbar */}
-            <Container disableGutters sx={{ position: 'relative' }}>
+            <Container 
+              disableGutters 
+              sx={{ position: 'relative' }}>
                 <Line />
             </Container>
             {/* Main Layout */}
-            <Container maxWidth='lg' disableGutters sx={{ backgroundColor: '#FFFFFF', boxShadow: "0px 0px 5px 1px rgba(227, 224, 224, 1)" }}>
-                <Container maxWidth='breakpoint' sx={{ height: '100%', margin: '0 auto', boxShadow: 1 }}>
+            <Container 
+              maxWidth='lg' 
+              disableGutters 
+              sx={{ 
+                backgroundColor: '#FFFFFF', 
+                boxShadow: "0px 0px 5px 1px rgba(227, 224, 224, 1)" 
+              }}>
+                <Container 
+                  maxWidth='breakpoint' 
+                  sx={{ 
+                    height: '100%', 
+                    margin: '0 auto', 
+                    boxShadow: 1 
+                  }}>
                     <Info />
                     <Search />
                     <Navbar />
